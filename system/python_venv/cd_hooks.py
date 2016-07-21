e38001cbb09fbc0efdc4a1492925ad1109310850
Eduardo Tenório <embatbr@gmail.com>
2016-07-21 16:16:06 -0300

    Allowing global environments (once active, only changes when enters a local venv directory tree).
    Signed-off-by: Eduardo Tenório <embatbr@gmail.com>

--- system/python_venv/cd_hooks.py
+++ system/python_venv/cd_hooks.py
@@ -1,8 +1,9 @@
 HOME = '/home/embat'
 CATHO_SERVICES = '%s/data/professional/Catho/appletini/services' % HOME
 VIVAREAL_SERVICES = '%s/data/professional/VivaReal/solution' % HOME
+DJANGO_TUTO = '%s/data/workspace/django-tuto' % HOME
 
-hooks = {
+local_venvs = {
     '%s/business' % CATHO_SERVICES : 'aptn-business',
     '%s/presentation' % CATHO_SERVICES : 'aptn-presentation',
     '%s/storage' % CATHO_SERVICES : 'aptn-storage',
@@ -11,12 +12,28 @@ hooks = {
     '%s/sower' % VIVAREAL_SERVICES : 'vr-sower'
 }
 
+global_venvs = [
+    'tb_dev'
+]
+
 
 if __name__ == '__main__':
+    import sys
     import os, os.path
 
+    venv = ''
+    cur_venv = None
+
+    if hasattr(sys, 'real_prefix'):
+        cur_venv = os.path.basename(sys.prefix)
+
+    if cur_venv in global_venvs:
+        venv = 'global-venv'
+
     fullpath = '%s/' % os.getcwd()
-    for key in hooks:
+    for key in local_venvs:
         if fullpath.startswith(key):
-            print(hooks[key])
+            venv = local_venvs[key]
             break
+
+    print(venv)
