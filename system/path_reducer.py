5b1e0ea8f601f51dab633314e7b2a4c1e0d6937d
Eduardo Tenório <embatbr@gmail.com>
2016-08-13 04:02:05 -0300

    - Function show_prompt extrated from .bashrc. - Script path_reduce.py (from /the/path/to/foo to /t/p/t/f). Not in use.
    Signed-off-by: Eduardo Tenório <embatbr@gmail.com>

---
+++ system/path_reducer.py
@@ -0,0 +1,19 @@
+import sys
+
+
+pwd = sys.argv[1].strip()
+if pwd == '/':
+    print('/')
+else:
+    HOME = sys.argv[2].strip()
+    if pwd.startswith(HOME):
+        pwd = pwd.replace(HOME, '~', 1)
+
+    dirnames = pwd.split('/')
+    if not dirnames[0]:
+        del dirnames[0]
+
+    path_prompt = '/'.join([dirname[0] for dirname in dirnames if dirname])
+    if not pwd.startswith('~'):
+        path_prompt = '/%s' % path_prompt
+    print(path_prompt)
